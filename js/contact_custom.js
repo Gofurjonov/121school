b8ded0e48334909a6b791ef0efd814369b8af292
Gofurjonov <gafurjonovdavronbek@gmail.com>
2023-05-22 14:40:25 +0500

    121-schoolwebsite

---
+++ js/contact_custom.js
@@ -0,0 +1,226 @@
+
+$(document).ready(function()
+{
+	"use strict";
+
+	var hamb = $('.hamburger');
+	var header = $('.header');
+	var hambActive = false;
+	var menuActive = false;
+	var ctrl = new ScrollMagic.Controller();
+	var map;
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initHamburger();
+	initParallax();
+	initGoogleMap();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if(window.innerWidth < 992)
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		else
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		if(window.innerWidth > 991 && menuActive)
+		{
+			closeMenu();
+		}
+	}
+
+	/* 
+
+	3. Initialize Hamburger
+
+	*/
+
+	function initHamburger()
+	{
+		if($('.hamburger_container').length)
+		{
+			var hamb = $('.hamburger_container');
+
+			hamb.on('click', function(event)
+			{
+				event.stopPropagation();
+
+				if(!menuActive)
+				{
+					openMenu();
+					
+					$(document).one('click', function cls(e)
+					{
+						if($(e.target).hasClass('menu_mm'))
+						{
+							$(document).one('click', cls);
+						}
+						else
+						{
+							closeMenu();
+						}
+					});
+				}
+				else
+				{
+					$('.menu_container').removeClass('active');
+					menuActive = false;
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		var fs = $('.menu_container');
+		fs.addClass('active');
+		hambActive = true;
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		var fs = $('.menu_container');
+		fs.removeClass('active');
+		hambActive = false;
+		menuActive = false;
+	}
+
+	/* 
+
+	4. Init Parallax
+
+	*/
+
+	function initParallax()
+	{
+		// Add parallax effect to home slider
+		if($('.slider_prlx').length)
+		{
+			var homeBcg = $('.slider_prlx');
+
+			var homeBcgScene = new ScrollMagic.Scene({
+		        triggerElement: homeBcg,
+		        triggerHook: 1,
+		        duration: "100%"
+		    })
+		    .setTween(TweenMax.to(homeBcg, 1, {y: '15%', ease:Power0.easeNone}))
+		    .addTo(ctrl);
+		}
+
+		// Add parallax effect to every element with class prlx
+		// Add class prlx_parent to the parent of the element
+		if($('.prlx_parent').length && $('.prlx').length)
+		{
+			var elements = $('.prlx_parent');
+
+			elements.each(function()
+			{
+				var ele = this;
+				var bcg = $(ele).find('.prlx');
+
+				var slideParallaxScene = new ScrollMagic.Scene({
+			        triggerElement: ele,
+			        triggerHook: 1,
+			        duration: "200%"
+			    })
+			    .setTween(TweenMax.from(bcg, 1, {y: '-30%', ease:Power0.easeNone}))
+			    .addTo(ctrl);
+			});
+		}
+	}
+
+	/* 
+
+	5. Init Google Map
+
+	*/
+
+	function initGoogleMap()
+	{
+		var myLatlng = new google.maps.LatLng(25.498345,-80.352984);
+    	var mapOptions = 
+    	{
+    		center: myLatlng,
+	       	zoom: 13,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			draggable: true,
+			scrollwheel: false,
+			zoomControl: true,
+			zoomControlOptions:
+			{
+				position: google.maps.ControlPosition.RIGHT_CENTER
+			},
+			mapTypeControl: false,
+			scaleControl: false,
+			streetViewControl: false,
+			rotateControl: false,
+			fullscreenControl: true,
+			styles:[]
+    	}
+
+    	// Initialize a map with options
+    	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    	map.panBy(-45, 0);
+
+		// Re-center map after window resize
+		google.maps.event.addDomListener(window, 'resize', function()
+		{
+			setTimeout(function()
+			{
+				google.maps.event.trigger(map, "resize");
+				map.setCenter(myLatlng);
+			}, 1400);
+		});
+	}
+});
+const Ismi = document.querySelector('#contact_form_name');
+const Email = document.querySelector('#contact_form_email');
+const Xabar = document.querySelector('#contact_form_message');
+function sendTelegram(){
+  let text="<strong>Ismi: </strong>"+Ismi.value+"%0A<strong>Email: </strong>"+Email.value+"%0A<strong>Xabar: </strong>"+Xabar.value;
+	console.log(text);
+  let theUrl ="https://api.telegram.org/bot5888984594:AAEMLeJDmA4Niee-Fo9XlOhtLhQpqq3aX18/sendMessage?chat_id=5036956072&parse_mode=html&text=" + text;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send( null );
+  Ismi.value='';
+  Email.value='';S
+  Xabar.value='';
+  subject.value='';
+};
+
+
